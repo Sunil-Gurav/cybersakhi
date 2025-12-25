c75c6de704dfb96d16fa2b46539c2f5c25f63b2a
Sunil-Gurav <sunilgurav6366@gmail.com>
2025-12-25 16:49:15 +0530

    upadted

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -1,7 +1,7 @@
 // backend/middleware/authMiddleware.js
 
 import jwt from "jsonwebtoken";
-import User from "../models/user.js";
+import User from "../models/User.js";
 
 export const protect = async(req, res, next) => {
     try {
